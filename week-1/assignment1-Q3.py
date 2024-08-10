1b4f9d5c9e7d48d5be31dad8c4c2efa3f5be9fff
Tripura kant <tripura.kant@zoom.us>
2024-08-11 02:02:35 +0530

    new 2024-08-11

---
+++ week-1/assignment1-Q3.py
@@ -0,0 +1,9 @@
+# Ask 3 numbers from User and Calculate the Average.
+
+num1 = int(input("Enter num1 "))
+num2 = int(input("Enter num2 "))
+num3 = int(input("Enter num3 "))
+
+average = (num1 + num2 + num3) / 3
+
+print(average)
---
+++ week-1/assignment1-Q4.py
@@ -0,0 +1,1 @@
+# Ask value in Rupees and Convert into Paise.
