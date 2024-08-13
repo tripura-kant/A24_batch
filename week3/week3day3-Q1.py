e869e69670d56f42221f65bd91a3653e3961476c
Tripura kant <tripura.kant@zoom.us>
2024-08-13 13:58:08 +0530

    new 2024-08-13

--- week3/week3day3-Q1.py
+++ week3/week3day3-Q1.py
@@ -1,4 +1,16 @@
 n = 5
 
-for i in range(1, n+1):
-    for j in range(1, n-1+1)
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+    for k in range(1, i + 1):
+        print(k, end=" ")
+    print()
+
+# n = int(input("Enter number of lines = "))
+# for i in range(1, n + 1):
+#     for j in range(1, n - i + 1):
+#         print(" ", end=" ")
+#     for k in range(1, i + 1):
+#         print(k, end=" ")
+#     print()
