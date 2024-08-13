8deb9bad4fc8a3e3d0f26beaecd20f575885fca3
Tripura kant <tripurakant@gmail.com>
2024-08-13 23:58:07 +0530

    new 2024-08-13

--- week3/week3day3-Q10.py
+++ week3/week3day3-Q10.py
@@ -3,11 +3,11 @@ for i in range(1, (n // 2 + 1) + 1):
     for j in range(1, (n // 2 + 1) - i + 1):
         print(" ", end=" ")
     for k in range(1, 2 * i):
-        print("*", end=" ")
+        print(k, end=" ")
     print()
 for i in range(n // 2, 0, -1):
     for j in range(1, (n // 2 + 1) - i + 1):
         print(" ", end=" ")
     for k in range(1, 2 * i):
-        print("*", end=" ")
+        print(k, end=" ")
     print()
---
+++ week3/week3day3-Q7.py
@@ -0,0 +1,7 @@
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print(n + 1 - i, end=" ")
+    print()
