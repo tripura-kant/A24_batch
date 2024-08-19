78d1f404c646989a628b6ce64bab23ff98e665ee
Tripura kant <tripurakant@gmail.com>
2024-08-19 23:50:43 +0530

    new 2024-08-19

---
+++ week7/week7day3-Q2.py
@@ -0,0 +1,16 @@
+# Armstrong Nuumber
+
+def check_Arm(num):
+    n = num
+    nod = len(str(n))
+    result = 0
+
+    while n > 0:
+        ld = n % 10
+        result = result + (ld ** nod)
+        n = n // 10
+    return result == num
+
+
+num = 347
+print(check_Arm(num))
