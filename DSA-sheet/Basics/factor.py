f24837bf7f1961d0eff94ceba6086b63300b0f48
Tripura kant <tripurakant@gmail.com>
2024-09-08 23:58:01 +0530

    new 2024-09-08

--- DSA-sheet/Basics/factor.py
+++ DSA-sheet/Basics/factor.py
@@ -1,8 +1,18 @@
-num = 20
+num = 17
 result = []
 
+# Brute force
 for i in range(1, num + 1):
     if num % i == 0:
         result.append(i)
 
 print(result)
+
+# Optimal
+# for i in range(1, num // 2):
+#     if num % i == 0:
+#         result.append(i)
+#
+# result.append(num)
+#
+# print(result)
