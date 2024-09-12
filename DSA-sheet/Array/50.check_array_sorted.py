47d097ae9dcd269a84decdc2fe7e81f997b2d883
Tripura kant <tripurakant@gmail.com>
2024-09-13 00:38:12 +0530

    new 2024-09-13

--- DSA-sheet/Array/50.check_array_sorted.py
+++ DSA-sheet/Array/50.check_array_sorted.py
@@ -1,3 +1,17 @@
 # Check if array is sorted and rotated
 
 arr = [2, 4, 9, 11, 14]
+
+
+def check_sorted_rotated(arr):
+    n = len(arr)
+    rotation = 0
+    for i in range(0, n - 1):
+        if arr[i] > arr[(i + 1) % n]:
+            rotation += 1
+        if rotation > 1:
+            return False
+    return True
+
+
+print(check_sorted_rotated(arr))
