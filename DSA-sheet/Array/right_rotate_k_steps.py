91c05278789c506c15e94b598d6da2bfed0fc4bc
Tripura kant <tripurakant@gmail.com>
2024-09-17 22:58:19 +0530

    new 2024-09-17

--- DSA-sheet/Array/right_rotate_k_steps.py
+++ DSA-sheet/Array/right_rotate_k_steps.py
@@ -1,6 +1,13 @@
-nums = [5, 6, 1, 2]
+nums = [5, 6, 1, 2, 2]
+n = len(nums)
 k = 3
-for _ in range(k):
+# for _ in range(k):
+#     elem = nums.pop()
+#     nums.insert(0, elem)
+#
+# print(nums)
+rotatation = k % n
+for _ in range(rotatation):
     elem = nums.pop()
     nums.insert(0, elem)
 
