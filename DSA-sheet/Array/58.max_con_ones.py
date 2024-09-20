07ab36974524b84000d2e2178d62910b1b3cc8b1
Tripura kant <tripurakant@gmail.com>
2024-09-20 22:53:16 +0530

    new 2024-09-20

---
+++ DSA-sheet/Array/58.max_con_ones.py
@@ -0,0 +1,20 @@
+# Maximum Consecutive Ones
+
+arr = [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0]
+
+
+def max_one(arr):
+    max_count = 0
+    count = 0
+
+    for i in range(0, len(arr)):
+        if arr[i] == 1:
+            count += 1
+        else:
+            max_count = max(max_count, count)
+            count = 0
+
+    return max(max_count, count)
+
+
+print(max_one(arr))
