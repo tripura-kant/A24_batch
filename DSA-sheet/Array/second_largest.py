82096b3b570af17564b8a268436713dcc13efd83
Tripura kant <tripurakant@gmail.com>
2024-10-06 21:22:26 +0530

    new 2024-10-06

--- DSA-sheet/Array/second_largest.py
+++ DSA-sheet/Array/second_largest.py
@@ -1,3 +1,30 @@
 #
 
 arr = [5, -10, 13, 67, 89, -100, 55]
+
+
+def find_second_max(arr):
+    maxi = float('-inf')
+    mini = float('inf')
+    n = len(arr)
+    sec_max = float('-inf')
+    sec_mini = float('inf')
+    for i in range(0, n):
+        if arr[i] > maxi:
+            sec_max = maxi
+            maxi = arr[i]
+
+        elif arr[i] > sec_max and arr[i] != maxi:
+            maxi = arr[i]
+
+        elif arr[i] < mini:
+            sec_mini = mini
+            mini = arr[i]
+
+        elif arr[i] < sec_mini and arr[i] != mini:
+            sec_mini = arr[i]
+
+    return maxi, mini, sec_mini, sec_max
+
+
+print(find_second_max(arr))
