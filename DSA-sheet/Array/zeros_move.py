1702d7a675f4d680f76f8a7651ba5716247c5281
Tripura kant <tripurakant@gmail.com>
2024-10-27 00:47:41 +0530

    new 2024-10-27

---
+++ DSA-sheet/Array/zeros_move.py
@@ -0,0 +1,15 @@
+nums = [0, 1, 0, 3, 12]
+non_zero_list = []
+n = len(nums)
+for i in range(0, n):
+    if nums[i] != 0:
+        non_zero_list.append(nums[i])
+
+print(non_zero_list)
+n_z_l = len(non_zero_list)
+
+for i in range(0, n_z_l):
+    nums[i] = non_zero_list[i]
+for i in range(n_z_l, n):
+    nums[i] = 0
+print(nums)
