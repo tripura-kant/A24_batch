c916977c1d9ac184ab732e8c38f7f11865828d0e
Tripura kant <tripurakant@gmail.com>
2024-10-27 22:58:24 +0530

    new 2024-10-27

--- DSA-sheet/Array/57_missing_num.py
+++ DSA-sheet/Array/57_missing_num.py
@@ -1,18 +1,20 @@
-arr = [4, 3, 1, 0]
+arr = [4, 3, 1, 0, 2, 5, 6, 7, 9]
 # for i in range(0, len(arr) + 1):
 #     if i not in arr:
 #         print(i)
 
 
 n = len(arr)
-
-dict = {}
-
-for i in range(0, n):
-    dict[i] = 0
-
-for i in range(0, n):
-    dict[arr[i]] = 1
-for k, v in dict.items():
-    if v == 0:
-        print(k)
+#
+# dict = {}
+#
+# for i in range(0, n):
+#     dict[i] = 0
+#
+# for i in range(0, n):
+#     dict[arr[i]] = 1
+# for k, v in dict.items():
+#     if v == 0:
+#         print(k)
+original_total = (n * (n + 1)) // 2
+print(original_total - sum(arr))
