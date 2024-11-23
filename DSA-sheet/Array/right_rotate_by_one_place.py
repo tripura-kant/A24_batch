7f346370b0fa11347cfe0dab9d5a171647348c38
Tripura kant <tripurakant@gmail.com>
2024-11-23 16:58:02 +0530

    new 2024-11-23

--- DSA-sheet/Array/right_rotate_by_one_place.py
+++ DSA-sheet/Array/right_rotate_by_one_place.py
@@ -1,7 +1,16 @@
-nums = [5, 9, 8, 2, 1]
+nums = [5, 6, 1, 8, 3, 4, 2, 9]
 
 elem = nums.pop()
 
 nums.insert(0, elem)
 
 print(nums)
+
+#
+# k = 3
+#
+# for _ in range(k):
+#     ele = nums.pop()
+#     nums.insert(0, ele)
+#
+# print(nums)
