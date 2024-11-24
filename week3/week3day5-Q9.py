7c68cfb278324c41f32be5e1dcb4dd448383d0e2
Tripura kant <tripurakant@gmail.com>
2024-11-24 17:03:08 +0530

    new 2024-11-24

--- week3/week3day5-Q9.py
+++ week3/week3day5-Q9.py
@@ -1,4 +1,20 @@
-my_list = [5, -8, 10, -15, 2, -4, 95, -34, 25]
+def is_prime_number(num: int) -> bool:
+    # I have not used factors logic here
+    # I will be explaning this in next lecture
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
 
-for i in range(0, len(my_list)):
-    if my_list[i] 
+
+my_list = [4, 8, 6, 19, 3, 12, 1, 7, 6, 2]
+max_prime_number = 0
+for num in my_list:
+    if is_prime_number(num):
+        if num > max_prime_number:
+            max_prime_number = num
+
+if max_prime_number != 0:
+    print(f"Largest prime number = {max_prime_number}")
+else:
+    print("No prime numbers present in the list")
