a086cdf624ad2f07a89459838d8313427703a09b
Tripura kant <tripurakant@gmail.com>
2024-11-24 16:55:54 +0530

    new 2024-11-24

--- week3/week3day5-Q7.py
+++ week3/week3day5-Q7.py
@@ -1,1 +1,9 @@
-my_list = [4, 8, 6, 5, 3, 12, 1, 7, 6, 20, 15, 25]
+my_list = [5, -8, 10, -15, 2, -4, 95, -34, 25]
+
+n = len(my_list)
+maxi = -float('inf')
+for num in range(0, n):
+    if my_list[num] > maxi:
+        maxi = my_list[num]
+
+print(maxi)
