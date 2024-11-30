866482c6c67e6f7c816526f900f177356d3513f2
Tripura kant <tripurakant@gmail.com>
2024-11-30 19:06:34 +0530

    new 2024-11-30

--- DSA-sheet/Array/59_single_num.py
+++ DSA-sheet/Array/59_single_num.py
@@ -1,11 +1,20 @@
 nums = [4, 1, 2, 1, 2]
 
-freq = {}
-for num in nums:
-    if num in freq:
-        freq[num] += 1
-    else:
-        freq[num] = 1
-for num in freq:
-    if freq[num] == 1:
-        print(num)
+# freq = {}
+# for num in nums:
+#     if num in freq:
+#         freq[num] += 1
+#     else:
+#         freq[num] = 1
+# for num in freq:
+#     if freq[num] == 1:
+#         print(num)
+n = len(nums)
+for i in range(0, n):
+    count = 0
+    for j in range(0, n):
+        if nums[j] == nums[i]:
+            count += 1
+
+    if count == 1:
+        print(nums[i])
