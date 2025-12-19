2ead6ba46522869ada8fb2f82a1ba6f62f50acf2
Tripura kant <tripura.kant@zoom.us>
2025-12-19 23:11:03 +0530

    Add

--- week3/week3day1-Q1.py
+++ week3/week3day1-Q1.py
@@ -1,9 +1,9 @@
-    def pattern(n: int):
-        for i in range(1, n + 1):
-            for j in range(1, 6):
-                print(1, end=" ")
-            print()
+def pattern(n: int):
+    for i in range(1, n + 1):
+        for j in range(1, 6):
+            print(1, end=" ")
+        print()
 
 
-    n = 5
-    pattern(n)
+n = 5
+pattern(n)
