c159972878a74363597307ffaa99e0d21c0aa9f5
Tripura kant <tripura.kant@zoom.us>
2025-12-21 15:56:28 +0530

    Add

--- week4/week4day1-Q2.py
+++ week4/week4day1-Q2.py
@@ -1,15 +1,29 @@
-# Write a function that takes a string and returns it in uppercase.
+# Make a list. Write a simple program for addition of the 2nd element
+# from start and 2nd element from the end
 
-def uppercase(s):
-    uppercase_string = ""
-    for char in s:
-        if "a" <= char <= "z":
-            uppercase_string += chr(ord(char) - 32)
-        else:
-            uppercase_string += char
-    return uppercase_string
+def addition(list):
+    num1 = list[1]
+    num2 = list[-2]
+    result = num1 + num2
+    return result
+
+list = [1,2,3,4,3,22,2]
+print(addition(list))
 
 
-my_string = "aB677^&*hx"
 
-print(uppercase(my_string))            
+# # Write a function that takes a string and returns it in uppercase.
+
+# def uppercase(s):
+#     uppercase_string = ""
+#     for char in s:
+#         if "a" <= char <= "z":
+#             uppercase_string += chr(ord(char) - 32)
+#         else:
+#             uppercase_string += char
+#     return uppercase_string
+
+
+# my_string = "aB677^&*hx"
+
+# print(uppercase(my_string))            
