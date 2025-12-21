8423a725fca1fe39bf26b168ac5421ae532a5c0f
Tripura kant <tripura.kant@zoom.us>
2025-12-21 17:56:28 +0530

    Add

--- week5/week5day1-Q3.py
+++ week5/week5day1-Q3.py
@@ -1,13 +1,26 @@
 #Write a function that removes all vowels (a, e, i, o, u) from a given
 # string?=
 
-def remove_vowels(s):
-    result = ""
+# def remove_vowels(s):
+#     result = ""
+#     for char in s:
+#         if char not in "aeiou":
+#             result += char
+
+#     return result
+
+# s = "adfsdfsdferw"
+# print(remove_vowels(s))
+
+
+def remove_vowels(s: str) -> str:
+    vowels = "aeiouAEIOU"
+    no_vowels = ""
     for char in s:
-        if char not in "aeiou":
-            result += char
+        if char not in vowels:
+            no_vowels += char
+    return no_vowels
 
-    return result
 
-s = "adfsdfsdferw"
-print(remove_vowels(s))
+my_string = "abcdefghij"
+print(remove_vowels(my_string))
--- week5/week5day1-Q4.py
+++ week5/week5day1-Q4.py
@@ -1,10 +1,28 @@
-#  Write a function that counts the number of words in a given string.
-# (String may only contain alphabets and spaces)
+# Write a function that counts the number of words in a given string.
+# (String may only contain alphabets and spaces
 
-def count_words(s):
-    return len(s.strip().split())
 
-s = "Hello world is Python"
-print(count_words(s))
+my_string = "python is easy to learn"
+
+
+def count_words(my_string):
+    count = 0
+    result = my_string.split()
+    for word in result:
+        count += 1  
+    return count
+print(count_words(my_string))        
+
+
+
+
+
+
+
+# def count_words(s):
+#     return len(s.strip().split())
+
+# s = "Hello world is Python"
+# print(count_words(s))
 
 
