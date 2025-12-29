aa94879fb76ef64b6b9c683f9e320445eac2ecbf
Tripura kant <tripura.kant@zoom.us>
2025-12-29 14:51:47 +0530

    Add

--- a4-batch-python-dsa/12. Split and Join/count-freq.py
+++ a4-batch-python-dsa/12. Split and Join/count-freq.py
@@ -6,7 +6,11 @@
 words_list = "apple mango apple orange"
 count_freq = {}
 
+# for word in words_list.split():
+#     count_freq[word] = count_freq.get(word, 0) + 1
+
+# print(count_freq)
+
 for word in words_list.split():
     count_freq[word] = count_freq.get(word, 0) + 1
-
 print(count_freq)
