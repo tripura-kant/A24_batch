19741f9874b63604e5eb73c5cb3d9f32851d9aee
Tripura kant <tripura.kant@zoom.us>
2025-12-30 19:11:27 +0530

    Add

--- week5/os.py
+++ week5/os.py
@@ -1,5 +1,5 @@
 import os
 
-for i in range(1, 11):
+for i in range(1, 12):
     filename = f"week5day2-Q{i}.py"
     os.open(filename, os.O_CREAT | os.O_APPEND)
---
+++ week5/week5day2-Q11.py
@@ -0,0 +1,21 @@
+# Given a dictionary with key-value pairs, remove all the keys with
+# values greater than K, including mixed values.
+# Input : test_dict = {‘Gfg’ : 3, ‘is’ : 7, ‘best’ : 10, ‘for’ : 6, ‘xyzx’ : ‘CS’}, K = 7
+# Output : {‘Gfg’ : 3, ‘for’ : 6, ‘xyzx’ : ‘CS’}
+# Explanation : All values greater than K are removed. Mixed value is
+# retained.
+# Input : test_dict = {‘Gfg’ : 3, ‘is’ : 7, ‘best’ : 10, ‘for’ : 6, ‘qqqq’ : ‘CS’}, K = 1
+# Output : {‘qqqq’ : ‘CS’}
+# Explanation : Only Mixed value is retained.
+
+test_dict = {'Gfg' : 3, 'is' : 7, 'best' : 10, 'for' : 6, 'xyzx' : 'CS'}
+
+k = 7
+res = {}
+
+for i in test_dict:
+    print(test_dict[i])
+    if test_dict[i] < int(k):
+        res[i] = res.get(i, 0) + test_dict[i]
+        print(f"KEY IS {i} Value is {test_dict[i]}")
+    else:
