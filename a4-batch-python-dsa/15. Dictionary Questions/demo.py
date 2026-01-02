481de02cfc42644e47d15912328ee6845ea5951f
Tripura kant <tripura.kant@zoom.us>
2026-01-02 14:51:43 +0530

    Add

--- a4-batch-python-dsa/15. Dictionary Questions/demo.py
+++ a4-batch-python-dsa/15. Dictionary Questions/demo.py
@@ -7,11 +7,12 @@ max_elem = 0
 for key in my_list:
     result[key] = result.get(key, 0 ) + 1
 
-# print(result)    
+
 for elem in result:
-    if result[key] > max_freq:
-        max_freq = result[key]
+    if result[elem] > max_freq:
+        max_freq = result[elem]
         max_elem = elem
 
 print(max_freq)
 print(max_elem)
+
