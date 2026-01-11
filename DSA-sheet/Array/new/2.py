0ba8439afba9c7258ae94dced6bae911002d1c2d
Tripura kant <tripura.kant@zoom.us>
2026-01-11 18:11:01 +0530

    Add

--- DSA-sheet/Array/new/2.py
+++ DSA-sheet/Array/new/2.py
@@ -1,7 +1,19 @@
 #Find the second largest element in an array
 
 
-nums = [23,2,-97,99,67]
+nums = [23,2,-97,99,67, 88]
 
-result = float("-inf")
+largest = float("-inf")
 
+sec_largest = float("-inf")
+
+for num in nums:
+    if num > largest:
+        largest = num
+
+for num in nums:
+    if num < largest and num > sec_largest:
+        sec_largest = num        
+
+print(largest)
+print(sec_largest)
