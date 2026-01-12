d963d65e6a351aa046d884135ffa24e5e9912336
Tripura kant <tripura.kant@zoom.us>
2026-01-12 09:11:00 +0530

    Add

--- DSA-sheet/Array/new/4.py
+++ DSA-sheet/Array/new/4.py
@@ -1,4 +1,21 @@
 # Remove Duplicates from a Sorted Array
 
+#phle dictionary freq map banao fir j zeo leke nums ko change kro aur j ka count    
+# nums = [23,24,499,2]
 
-nums = [23,24,499]
+nums = [23,23,24,499, 560,999, 23, 24]
+
+n = len(nums)
+freq_map={}
+for num in nums:
+    freq_map[num] = freq_map.get(num, 0) + 1 
+print(freq_map)
+
+j = 0
+for k in freq_map:
+    nums[j] = k
+    j += 1
+
+print(j)
+
+print(nums)
