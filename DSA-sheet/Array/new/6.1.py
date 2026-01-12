66abf560001f48aa9406ec820584576d8d133a2c
Tripura kant <tripura.kant@zoom.us>
2026-01-12 17:51:00 +0530

    Add

--- DSA-sheet/Array/new/6.1.py
+++ DSA-sheet/Array/new/6.1.py
@@ -1,9 +1,12 @@
 # Rotate k times
 
 nums = [1, 2, 3, 4, 5, 6, 7]
-k = 3
 
-for _ in range(0, k):
+n = len(nums)
+k = 3
+rotation = k % n
+
+for _ in range(0, rotation):
     e = nums.pop()
     nums.insert(0, e)
 
