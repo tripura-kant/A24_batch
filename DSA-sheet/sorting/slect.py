3bf6280b916a9004342c8b17f090ea54e87a565f
Tripura kant <tripura.kant@zoom.us>
2026-02-12 13:51:44 +0530

    Add

--- DSA-sheet/sorting/slect.py
+++ DSA-sheet/sorting/slect.py
@@ -1,2 +1,19 @@
 # Selection Sort
 
+
+nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+def selection(nums):
+    n = len(nums)
+    
+    for i in range(0, n):
+        min_index = i
+        for j in range(i+1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+
+
+    return nums
+
+result = selection(nums)
+print(result)            
