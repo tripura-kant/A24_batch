956b4c224437e5f08934c87021e6cf7bb18fc9b1
Tripura kant <tripura.kant@zoom.us>
2026-06-20 20:49:06 +0530

    commit

--- DSA-sheet/Basics/recursion/reverse_recur.py
+++ DSA-sheet/Basics/recursion/reverse_recur.py
@@ -1,1 +1,15 @@
 # reverse
+
+n = [1,2,5,6,9]
+def reverse_array(n):
+    left = 0
+    right = len(n)-1
+    while left < right:
+        n[left], n[right] = n[right], n[left]
+        left += 1
+        right -= 1
+    return n
+
+result = reverse_array(n)
+print(result)
+
