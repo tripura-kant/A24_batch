3ae69643aa42f273d3e21336f693dee351e09a88
Tripura kant <tripura.kant@zoom.us>
2026-06-20 20:39:03 +0530

    commit

--- DSA-sheet/Basics/recursion/print1ton.py
+++ DSA-sheet/Basics/recursion/print1ton.py
@@ -1,2 +1,12 @@
 # Print 1 to N using recursion
 
+N = 5
+i = 1
+def func(i,N):
+    if i > N:
+        return
+    print(i)
+    func(i+1,N)
+
+func(i,N)
+
--- git.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/tripurakant/Documents/code/My_git/A24_batch
-
-git add .
-git commit -m "Add"
-git push
